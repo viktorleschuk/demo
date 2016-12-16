a196bbab34ffcdfcc799b87fdd3f692b11073965
vitiok <leschuk@ex.ua>
2016-12-16 17:21:28 +0200

    Added PHPDoc blocks

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
+/**
+ * Class IndexController
+ * @package App\Http\Controllers
+ */
 class IndexController extends Controller
 {
+    /**
+     * Returned users with last review and reviews_count
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function __invoke()
     {
         $users = User::withCount('reviews')->get();
